2175199aad57e3a9feec7cff44de4d8e56baab43
dstamos <dimitrios.i.stamos@gmail.com>
2020-11-10 22:33:22 +0000

    Preliminary data stuff.

--- main.py
+++ main.py
@@ -56,103 +56,51 @@ def main():
 
     from src.ltl import BiasLTL
     from src.utilities import multiple_tasks_mse
-    from sklearn.model_selection import train_test_split
 
-    training_tasks_pct = data_settings['training_tasks_pct']
-    validation_tasks_pct = data_settings['validation_tasks_pct']
-    test_tasks_pct = data_settings['test_tasks_pct']
-    training_tasks_indexes, temp_indexes = train_test_split(range(len(all_features)), test_size=1 - training_tasks_pct, shuffle=True)
-    validation_tasks_indexes, test_tasks_indexes = train_test_split(temp_indexes, test_size=test_tasks_pct / (test_tasks_pct + validation_tasks_pct))
+    from src.data_management import split_data
+    data = split_data(all_features, all_labels, data_settings)
 
-    # Training task data
-    training_tasks_training_features = [all_features[i] for i in training_tasks_indexes]
-    training_tasks_training_labels = [all_labels[i] for i in training_tasks_indexes]
-    point_indexes_per_training_task = [idx * np.ones(all_features[task_idx].shape[0]) for idx, task_idx in enumerate(training_tasks_indexes)]
-
-    training_tasks_training_features = np.concatenate(training_tasks_training_features)
-    training_tasks_training_labels = np.concatenate(training_tasks_training_labels)
-    point_indexes_per_training_task = np.concatenate(point_indexes_per_training_task).astype(int)
-
-    # Validation task data
-    validation_tasks_training_features = [all_features[i] for i in validation_tasks_indexes]
-    validation_tasks_training_labels = [all_labels[i] for i in validation_tasks_indexes]
-    point_indexes_per_validation_task = [idx * np.ones(all_features[task_idx].shape[0]) for idx, task_idx in enumerate(validation_tasks_indexes)]
-
-    validation_tasks_training_features = np.concatenate(validation_tasks_training_features)
-    validation_tasks_training_labels = np.concatenate(validation_tasks_training_labels)
-    point_indexes_per_validation_task = np.concatenate(point_indexes_per_validation_task).astype(int)
-
-    validation_tasks_test_features = [all_features[i] for i in validation_tasks_indexes]
-    validation_tasks_test_labels = [all_labels[i] for i in validation_tasks_indexes]
-    validation_tasks_test_features = np.concatenate(validation_tasks_test_features)
-    # Test task data
-    test_tasks_training_features = [all_features[i] for i in test_tasks_indexes]
-    test_tasks_training_labels = [all_labels[i] for i in test_tasks_indexes]
-    point_indexes_per_test_task = [idx * np.ones(all_features[task_idx].shape[0]) for idx, task_idx in enumerate(test_tasks_indexes)]
-
-    test_tasks_training_features = np.concatenate(test_tasks_training_features)
-    test_tasks_training_labels = np.concatenate(test_tasks_training_labels)
-    point_indexes_per_test_task = np.concatenate(point_indexes_per_test_task).astype(int)
-
-    test_tasks_test_features = [all_features[i] for i in test_tasks_indexes]
-    test_tasks_test_labels = [all_labels[i] for i in test_tasks_indexes]
-    test_tasks_test_features = np.concatenate(test_tasks_test_features)
-    """
-    Optimize metaparameter on the training data of the training tasks
-    """
+    # Training
     model_ltl = BiasLTL(regularization_parameter=1e-1, step_size_bit=1e+3)
-    model_ltl.fit(training_tasks_training_features, training_tasks_training_labels, {'point_indexes_per_task': point_indexes_per_training_task})
+    model_ltl.fit(data['training_tasks_training_features'], data['training_tasks_training_labels'], {'point_indexes_per_task': data['point_indexes_per_training_task']})
 
-    """
-    Optimize the weight vectors on the training data of the target tasks (those should be training data from the validation or test tasks)
-    Passing predictions_for_each_training_task as True, recovers the weight vectors for each metaparameter that was returned during the training (one for each training task).        
-    """
-    extra_inputs = {'predictions_for_each_training_task': False, 'point_indexes_per_task': point_indexes_per_validation_task}
-    weight_vectors_per_task = model_ltl.fit_inner(validation_tasks_training_features,
-                                                  all_labels=validation_tasks_training_labels,
+    # Validation
+    extra_inputs = {'predictions_for_each_training_task': False, 'point_indexes_per_task': data['point_indexes_per_validation_task']}
+    weight_vectors_per_task = model_ltl.fit_inner(data['validation_tasks_training_features'],
+                                                  all_labels=data['validation_tasks_training_labels'],
                                                   extra_inputs=extra_inputs)
-    """
-    Take the weight vectors from the previous step and make predictions on the test data of the same tasks you optimized them on
-    """
+    predictions_validation = model_ltl.predict(data['validation_tasks_test_features'], weight_vectors_per_task, extra_inputs=extra_inputs)
 
-    predictions_validation = model_ltl.predict(validation_tasks_test_features, weight_vectors_per_task, extra_inputs=extra_inputs)
-
-    val_performance = multiple_tasks_mse(validation_tasks_test_labels, predictions_validation, extra_inputs['predictions_for_each_training_task'])
+    val_performance = multiple_tasks_mse(data['validation_tasks_test_labels'], predictions_validation, extra_inputs['predictions_for_each_training_task'])
     print(val_performance)
 
-    """
-    Optimize the weight vectors on the training data of the target tasks (those should be training data from the validation or test tasks)
-    Passing predictions_for_each_training_task as True, recovers the weight vectors for each metaparameter that was returned during the training (one for each training task).        
-    """
-    extra_inputs = {'predictions_for_each_training_task': True, 'point_indexes_per_task': point_indexes_per_test_task}
-    weight_vectors_per_task = model_ltl.fit_inner(test_tasks_training_features,
-                                                  all_labels=test_tasks_training_labels,
+    # Test
+    extra_inputs = {'predictions_for_each_training_task': True, 'point_indexes_per_task': data['point_indexes_per_test_task']}
+    weight_vectors_per_task = model_ltl.fit_inner(data['test_tasks_training_features'],
+                                                  all_labels=data['test_tasks_training_labels'],
                                                   extra_inputs=extra_inputs)
-    """
-    Take the weight vectors from the previous step and make predictions on the test data of the same tasks you optimized them on
-    """
 
-    predictions_test = model_ltl.predict(test_tasks_test_features, weight_vectors_per_task, extra_inputs=extra_inputs)
+    predictions_test = model_ltl.predict(data['test_tasks_test_features'], weight_vectors_per_task, extra_inputs=extra_inputs)
 
-    ltl_test_performance = multiple_tasks_mse(test_tasks_test_labels, predictions_test, extra_inputs['predictions_for_each_training_task'])
+    ltl_test_performance = multiple_tasks_mse(data['test_tasks_test_labels'], predictions_test, extra_inputs['predictions_for_each_training_task'])
     print(ltl_test_performance)
 
     ###################################################################################################
     from src.independent_learning import ITL
 
-    extra_inputs = {'point_indexes_per_task': point_indexes_per_test_task}
+    extra_inputs = {'point_indexes_per_task': data['point_indexes_per_test_task']}
 
     model_itl = ITL(regularization_parameter=1e-6)
-    model_itl.fit(test_tasks_training_features, test_tasks_training_labels, extra_inputs=extra_inputs)
+    model_itl.fit(data['test_tasks_training_features'], data['test_tasks_training_labels'], extra_inputs=extra_inputs)
 
-    predictions_validation = model_itl.predict(test_tasks_test_features, extra_inputs=extra_inputs)
+    predictions_validation = model_itl.predict(data['test_tasks_test_features'], extra_inputs=extra_inputs)
 
-    val_performance = multiple_tasks_mse(test_tasks_test_labels, predictions_validation)
+    val_performance = multiple_tasks_mse(data['test_tasks_test_labels'], predictions_validation)
     print(val_performance)
 
-    predictions_test = model_itl.predict(test_tasks_test_features, extra_inputs=extra_inputs)
+    predictions_test = model_itl.predict(data['test_tasks_test_features'], extra_inputs=extra_inputs)
 
-    itl_test_performance = multiple_tasks_mse(test_tasks_test_labels, predictions_test)
+    itl_test_performance = multiple_tasks_mse(data['test_tasks_test_labels'], predictions_test)
     print(itl_test_performance)
 
     ###################################################################################################
@@ -162,24 +110,6 @@ def main():
     plt.axhline(y=itl_test_performance, xmin=0, xmax=len(ltl_test_performance)-1, color='tab:blue', label='ITL')
     plt.show()
 
-
-
-    ###########
-    training_settings_itl = {'regularization_parameter_range': [10 ** float(i) for i in np.linspace(-12, 4, 20)],
-                             'method': 'ITL'}
-
-    results_itl = training(data, training_settings_itl)
-    ###########
-
-    import matplotlib.pyplot as plt
-    plt.axhline(y=results_itl['test_perfomance'], xmin=0, xmax=len(all_features) - 1, color='k', label='indipendent learning')
-    # plt.axhline(y=results_batch_ltl['best_test_performance'], xmin=0, xmax=len(all_features)-1, color='tab:red', label='batch ltl')
-    plt.plot(results_online_ltl['best_test_performances'], color='tab:blue', label='online ltl')
-    plt.legend()
-    plt.xlabel('number of training tasks')
-    plt.ylabel('performanance')
-    plt.pause(0.1)
-    plt.show()
     print('done')
 
 
--- src/data_management.py
+++ src/data_management.py
@@ -1,5 +1,6 @@
 from sklearn.model_selection import train_test_split
 from collections import namedtuple
+import numpy as np
 
 
 class DataHandler:
@@ -81,3 +82,96 @@ class DataHandler:
         self.training_tasks = dataset_splits(self.training_tasks_indexes)
         self.validation_tasks = dataset_splits(self.validation_tasks_indexes)
         self.test_tasks = dataset_splits(self.test_tasks_indexes)
+
+
+def split_data(all_features, all_labels, data_settings):
+    """
+    Training tasks only have training data.
+    Validation tasks only have training and test data.
+    Test tasks have training, validation and test data. The validation data are currently not used for metalearning, only for independent learning.
+    :param all_features: list of numpy arrays (n_points, dims)
+    :param all_labels:  list of numpy arrays (n_points, )
+    :param data_settings:  dict of settings
+    :return:
+    """
+    training_tasks_pct = data_settings['training_tasks_pct']
+    validation_tasks_pct = data_settings['validation_tasks_pct']
+    test_tasks_pct = data_settings['test_tasks_pct']
+    training_tasks_indexes, temp_indexes = train_test_split(range(len(all_features)), test_size=1 - training_tasks_pct, shuffle=True)
+    validation_tasks_indexes, test_tasks_indexes = train_test_split(temp_indexes, test_size=test_tasks_pct / (test_tasks_pct + validation_tasks_pct))
+
+    training_points_pct = data_settings['training_points_pct']
+    validation_points_pct = data_settings['validation_points_pct']
+    test_points_pct = data_settings['test_points_pct']
+
+    # Training tasks (only training data)
+    training_tasks_training_features = [all_features[i] for i in training_tasks_indexes]
+    training_tasks_training_labels = [all_labels[i] for i in training_tasks_indexes]
+    point_indexes_per_training_task = [idx * np.ones(all_features[task_idx].shape[0]) for idx, task_idx in enumerate(training_tasks_indexes)]
+
+    # Validation tasks (training and test data)
+    validation_tasks_training_features = []
+    validation_tasks_training_labels = []
+    validation_tasks_test_features = []
+    validation_tasks_test_labels = []
+    point_indexes_per_validation_task = []
+    for counter, task_index in enumerate(validation_tasks_indexes):
+        training_features, test_features, training_labels, test_labels = train_test_split(all_features[task_index], all_labels[task_index], test_size=1 - training_points_pct, shuffle=True)
+
+        validation_tasks_training_features.append(training_features)
+        validation_tasks_training_labels.append(test_features)
+        validation_tasks_test_features.append(training_labels)
+        validation_tasks_test_labels.append(test_labels)
+        point_indexes_per_validation_task.append(counter * np.ones(all_features[task_index].shape[0]))
+
+    # Test tasks (training, validation and test tasks)
+    test_tasks_training_features = []
+    test_tasks_training_labels = []
+    test_tasks_validation_features = []
+    test_tasks_validation_labels = []
+    test_tasks_test_features = []
+    test_tasks_test_labels = []
+    point_indexes_per_test_task = []
+    for counter, task_index in enumerate(validation_tasks_indexes):
+        training_features, temp_features, training_labels, temp_labels = train_test_split(all_features[task_index], all_labels[task_index], test_size=1 - training_points_pct, shuffle=True)
+        validation_features, test_features, validation_labels, test_labels = train_test_split(temp_features, temp_labels, test_size=test_points_pct / (test_points_pct + validation_points_pct))
+
+        test_tasks_training_features.append(training_features)
+        test_tasks_training_labels.append(test_features)
+        test_tasks_validation_features.append(training_labels)
+        test_tasks_validation_labels.append(test_labels)
+        test_tasks_test_features.append(training_labels)
+        test_tasks_test_labels.append(test_labels)
+        point_indexes_per_test_task.append(counter * np.ones(all_features[task_index].shape[0]))
+
+    data = {'training_tasks_indexes': training_tasks_indexes,
+            'validation_tasks_indexes': validation_tasks_indexes,
+            'test_tasks_indexes': test_tasks_indexes,
+            # Training tasks
+            'training_tasks_training_features': training_tasks_training_features,
+            'training_tasks_training_labels': training_tasks_training_labels,
+            'point_indexes_per_training_task': point_indexes_per_training_task,
+            # Validation tasks
+            'validation_tasks_training_features': validation_tasks_training_features,
+            'validation_tasks_training_labels': validation_tasks_training_labels,
+            'validation_tasks_test_features': validation_tasks_test_features,
+            'validation_tasks_test_labels': validation_tasks_test_labels,
+            'point_indexes_per_validation_task': point_indexes_per_validation_task,
+            # Test tasks
+            'test_tasks_training_features': test_tasks_training_features,
+            'test_tasks_training_labels': test_tasks_training_labels,
+            'test_tasks_test_features': test_tasks_test_features,
+            'test_tasks_test_labels': test_tasks_test_labels,
+            'point_indexes_per_test_task': point_indexes_per_test_task,
+            }
+    return data
+
+
+def concatenate_data(all_features, indexes_per_task,  all_labels=None):
+    all_features = np.concatenate(all_features)
+    indexes_per_task = np.concatenate(indexes_per_task).astype(int)
+    if all_labels is None:
+        return all_features, indexes_per_task
+    else:
+        all_labels = np.concatenate(all_labels)
+        return all_features, indexes_per_task, all_labels
