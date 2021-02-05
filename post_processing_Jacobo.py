dec3aa934c89a47ef6cba0e23ca361bb825af985
priack <52912107+priack@users.noreply.github.com>
2021-02-05 11:55:11 +0000

    Plot updating

--- post_processing_Jacobo.py
+++ post_processing_Jacobo.py
@@ -16,7 +16,7 @@ def confidence_interval(data, axis=0, confidence=0.95):
 def standard_error(data, axis=0, correct=False):
     m, se = np.nanmean(data, axis), sp.sem(data, axis)
     if correct:
-        se = se*np.sqrt(len(m)) /np.sqrt(len(m)*10)
+        se = se*np.sqrt(len(m)) /np.sqrt(len(m)*nSeed)
     return m, m - se, m + se
 
 def plotSE(data, x, color, label, correct=False):
@@ -30,40 +30,53 @@ def plotSE(data, x, color, label, correct=False):
 font = {'size': 20}
 matplotlib.rc('font', **font)
 
-source = 'Dimitri'
-dataset = 1
+source = 'validation'
+dataset = 2
 if source == 'Dimitri':
     if dataset==1:
         foldername = 'results/results-first_dataset_all_metrics'
     else:
         foldername = 'results/results-second_dataset_all_metrics'
     seed_range = range(10)
+    nSeed = len(seed_range)
     test_tasks_tr_points_pct = np.linspace(0.00, 0.8, 30)
     prec = '-tr_pct_{:0.4f}'
     correct = True
-else:
+elif source=='Jacobo':
     foldername = 'results-first_dataset'
     seed_range = [0]
     test_tasks_tr_points_pct = np.linspace(0.00, 0.8, 41)
     prec = '-tr_pct_{:0.2f}'
     correct = False
+elif source == 'validation':
+    if dataset == 1:
+        foldername = 'results/results-first_dataset_validation_corrected'
+    else:
+        foldername = 'results/results-second_dataset_validation_corrected'
+    seed_range =range(6)
+    nSeed = len(seed_range)
+    prec = '-tr_pct_{:0.4f}'
+    test_tasks_tr_points_pct = np.linspace(0.00, 0.5, 26)
+    correct = True
+
 
 nPoints = len(test_tasks_tr_points_pct)
 subjFolder = os.listdir(foldername)
 nSubj = len(subjFolder)
 conditions = ['naive', 'itl', 'single_task', 'meta']
-evaluations = ['MAE', 'MSE', 'MCA', 'CD']
+evaluations = ['MAE','MSE','MCA', 'CD']
 # merged:True/False, evaluations, conditions, subjects, points
 data = np.empty((2, len(evaluations), len(conditions), nSubj, nPoints))
 seeds = np.empty((len(seed_range), len(evaluations), len(conditions), nPoints))
 seeds[:] = np.nan
+fitness = 'MSE'
 for s in range(nSubj):
-    for mc, m in enumerate([True, False]):
+    for mc, m in enumerate([False, True]):
         for pc, p in enumerate(test_tasks_tr_points_pct):
             for seed in seed_range:
                 try:
                     filename = './' + foldername + '/' + subjFolder[s] + '/seed_' + str(seed) + prec.format(p)\
-                               + '-merge_test_' + str(m) + '.pckl'
+                               + '-merge_test_' + str(m) + '-fitness_' + fitness + '.pckl'
                     f = open(filename, "rb")
                     aux = pickle.load(f)
                     f.close()
@@ -71,8 +84,9 @@ for s in range(nSubj):
                         seeds[seed, :, i, pc] = aux['test_performance_' + conditions[i]]
                     seeds[seed, :, 3, pc] = aux['test_performance_meta'][-1, :]
                 except Exception as e:
-                    print('broken', e, seed)
+                    print('broken', s, m, p, seed)
                     continue
+        seeds[seeds == -.99] = np.nan
         data[mc, :, :, s, :] = np.nanmean(seeds, 0)
         seeds[:] = np.nan
 
@@ -80,16 +94,15 @@ colors = ['tab:grey', 'tab:red', 'tab:green', 'tab:blue']
 labels = ['Naive', 'ITL', 'Single', 'Meta']
 dpi = 100
 
+f = plt.figure(figsize=(1920 / dpi, 1080 / dpi), dpi=dpi)
 for i in range(2):
-    f = plt.figure(figsize=(1920 / dpi, 1080 / dpi), dpi=dpi)
-    for ev in range(len(evaluations)):
-        plt.subplot(2, 2, ev+1)
+    for evc, ev in enumerate([1, 3]):
+        plt.subplot(2, 2, i*2+evc+1)
         for cond in range(len(conditions)):
             plotSE(data[i, ev, cond, :, :], test_tasks_tr_points_pct, colors[cond], labels[cond], correct)
-            plt.xlim([0.02, 0.8])
+            plt.xlim([0.0, 0.5])
         plt.ylabel(evaluations[ev])
-    plt.tight_layout()
-    plt.legend(frameon=False)
-    plt.savefig('analysis/all_subj_SE_'+source+str(dataset)+'_merged_' + str(bool(i)) + '.png', pad_inches=0)
-    plt.close(f)
-k = 1
+plt.tight_layout()
+plt.legend(frameon=False)
+plt.savefig('analysis/all_subj_SE_'+source+str(dataset)+ '_' + fitness + '.png', pad_inches=0)
+plt.close(f)
